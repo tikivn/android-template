ae552b442a03d4d3d5f8e873440b91db96b39a25
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-09-20 17:13:53 +0700

    configure Timber

--- sample/src/main/java/vn/tiki/sample/SampleApp.java
+++ sample/src/main/java/vn/tiki/sample/SampleApp.java
@@ -3,9 +3,11 @@ package vn.tiki.sample;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
+import android.util.Log;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
 import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
 import vn.tiki.daggers.ActivityInjector;
 import vn.tiki.daggers.AppInjector;
 import vn.tiki.daggers.Daggers;
@@ -26,6 +28,27 @@ public class SampleApp extends Application implements AppInjector {
 
     configureDagger();
     configureFabric();
+    configureTimber();
+  }
+
+  private void configureTimber() {
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree());
+    } else {
+      Timber.plant(new Timber.Tree() {
+        @Override protected void log(int priority, String tag, String message, Throwable t) {
+          if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+            return;
+          }
+
+          Crashlytics.log(priority, tag, message);
+
+          if (t != null) {
+            Crashlytics.logException(t);
+          }
+        }
+      });
+    }
   }
 
   protected void configureDagger() {
