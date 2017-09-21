bf63a3c86c389ecc25f1de382e5e0fef7b23baf5
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-09-21 10:08:30 +0700

    implement load products and paging

--- backend/app/routes/product_routes.js
+++ backend/app/routes/product_routes.js
@@ -4,12 +4,29 @@ module.exports = (app, database) => {
   const db = database.collection('products');
   let products = [];
   app.get('/products', (req, res) => {
-    if (products.length > 0) {
-      res.send(products);
-    } else {
+    if (products.length === 0) {
       const fileContent = fs.readFileSync('app/data/products.json', 'utf-8');
-      products = JSON.parse(fileContent);
-      res.send(products);
+      products = JSON.parse(fileContent).map(product => ({
+        title: product.title,
+        price: product.price,
+        image: product.image,
+        description: product.description,
+      }));
     }
+
+    const page = parseInt(req.query.page, 10);
+    const perPage = parseInt(req.query.per_page, 10);
+    const startIndex = (page - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    const responseProducts = products.filter((_, index) => index >= startIndex && index < endIndex);
+
+    const total = products.length;
+    const lastPage = Math.ceil(total / perPage);
+    res.send({
+      total,
+      current_Page: page,
+      last_page: lastPage,
+      items: responseProducts,
+    });
   });
 };
