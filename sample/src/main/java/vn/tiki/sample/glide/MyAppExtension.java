7aa725dccdc3888b14fff64c81a4d2c52b1c0e4b
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-09-28 10:42:26 +0700

    add glide extension to add placeholder

---
+++ sample/src/main/java/vn/tiki/sample/glide/MyAppExtension.java
@@ -0,0 +1,21 @@
+package vn.tiki.sample.glide;
+
+import com.bumptech.glide.annotation.GlideExtension;
+import com.bumptech.glide.annotation.GlideOption;
+import com.bumptech.glide.request.RequestOptions;
+import vn.tiki.sample.R;
+
+@GlideExtension
+public class MyAppExtension {
+  private static final int MINI_THUMB_SIZE = 100;
+
+  private MyAppExtension() {
+  } // utility class
+
+  @GlideOption
+  public static void miniThumb(RequestOptions options) {
+    options
+        .fitCenter()
+        .placeholder(R.drawable.ic_placeholder);
+  }
+}
--- sample/src/main/java/vn/tiki/sample/glide/MyAppGlideModule.java
+++ sample/src/main/java/vn/tiki/sample/glide/MyAppGlideModule.java
@@ -1,4 +1,4 @@
-package vn.tiki.sample;
+package vn.tiki.sample.glide;
 
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
