7fa9290c4b0c5aef7e410aaf6505372becbebf0a
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-10-24 14:18:21 +0700

    add home

---
+++ sample/src/main/java/vn/tiki/sample/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package vn.tiki.sample.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import vn.tiki.sample.R
+
+class HomeActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_home)
+  }
+}
