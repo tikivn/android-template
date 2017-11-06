90ce53e0312c82bca2227a5cf0a602b6e94b745e
Giang Nguyen <giangnguyen.tale@gmail.com>
2017-11-06 15:12:39 +0700

    use instance of in TypeFactory instead of Map to fix subtype check

--- viewholders-compiler/src/main/java/vn/tiki/viewholders/compiler/TypeFactoryGenerator.java
+++ viewholders-compiler/src/main/java/vn/tiki/viewholders/compiler/TypeFactoryGenerator.java
@@ -1,15 +1,12 @@
 package vn.tiki.viewholders.compiler;
 
 import static javax.lang.model.element.Modifier.FINAL;
-import static javax.lang.model.element.Modifier.PRIVATE;
 import static javax.lang.model.element.Modifier.PUBLIC;
 
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.MethodSpec.Builder;
-import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 import java.util.List;
@@ -17,13 +14,10 @@ import java.util.List;
 class TypeFactoryGenerator {
 
   static final String NAME = "TypeFactoryImpl";
-  private final TypeName mapType;
   private final List<ViewHolderInfo> viewHolderInfoList;
 
   TypeFactoryGenerator(final List<ViewHolderInfo> viewHolderInfoList) {
     this.viewHolderInfoList = viewHolderInfoList;
-    ClassName map = ClassName.get("java.util", "LinkedHashMap");
-    mapType = ParameterizedTypeName.get(map, ClassName.get(Class.class), ClassName.get(Integer.class));
   }
 
   JavaFile brewJava() {
@@ -32,47 +26,42 @@ class TypeFactoryGenerator {
         .build();
   }
 
-  private MethodSpec createConstructor() {
-    final Builder result = MethodSpec.constructorBuilder()
-        .addStatement("typeMapping = new $T()", mapType);
-
-    for (ViewHolderInfo viewHolderInfo : viewHolderInfoList) {
-      result.addStatement(
-          "typeMapping.put($T.class, $L)",
-          viewHolderInfo.getModelType(),
-          viewHolderInfo.getLayout().code);
-    }
-    return result.build();
-  }
-
   private TypeSpec createType() {
     final ClassName typeFactoryType = ClassName.get("vn.tiki.noadapter2", "TypeFactory");
     TypeSpec.Builder result = TypeSpec.classBuilder(NAME)
         .addModifiers(FINAL)
-        .addSuperinterface(typeFactoryType)
-        .addField(mapType, "typeMapping", PRIVATE, FINAL);
+        .addSuperinterface(typeFactoryType);
 
-    result.addMethod(createConstructor());
     result.addMethod(createTypeOfMethod());
     return result.build();
   }
 
   private MethodSpec createTypeOfMethod() {
 
+    final CodeBlock.Builder ifStatementBuilder = CodeBlock.builder();
+
+    for (int i = 0; i < viewHolderInfoList.size(); i++) {
+      ViewHolderInfo viewHolderInfo = viewHolderInfoList.get(i);
+      if (i == 0) {
+        ifStatementBuilder.beginControlFlow("if (item instanceof $T)", viewHolderInfo.getModelType());
+      } else {
+        ifStatementBuilder.beginControlFlow("else if (item instanceof $T)", viewHolderInfo.getModelType());
+      }
+      ifStatementBuilder.addStatement("return $L", viewHolderInfo.getLayout().code)
+          .endControlFlow();
+    }
+
+    ifStatementBuilder
+        .beginControlFlow("else")
+        .addStatement("throw new $T(\"unknown \" + item.getClass())", IllegalArgumentException.class)
+        .endControlFlow();
+
     return MethodSpec.methodBuilder("typeOf")
         .addAnnotation(Override.class)
         .addModifiers(PUBLIC)
         .returns(TypeName.INT)
         .addParameter(Object.class, "item")
-        .addCode(
-            CodeBlock.builder()
-                .addStatement("final Class<?> itemClass = item.getClass()")
-                .add("if (typeMapping.containsKey(itemClass)) {\n")
-                .addStatement("return typeMapping.get(itemClass)")
-                .add("} else {\n")
-                .addStatement("throw new $T(\"unknown \" + itemClass)", IllegalArgumentException.class)
-                .add("}")
-                .build())
+        .addCode(ifStatementBuilder.build())
         .build();
   }
 }
